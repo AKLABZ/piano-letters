f77661a1796f27f5edc8dd603c1e5bb59bb19271
AKLABZ <133277560+AKLABZ@users.noreply.github.com>
2023-07-30 22:19:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// script.js
 const text = document.querySelector("h1");
 const button = document.querySelector(".button");
-
+const customCursor = document.querySelector(".custom-cursor");
+const cursorSquare = document.querySelector(".cursor-square");
+const mainPage = document.querySelector(".main-page"); // New letter button
+const exppandedCursor = document.querySelector(".exppanded-cursor");
+const transitionBg =  document.querySelector(".transition-bg");
 let isPaused = true;
 let isInitialized = false;
+let hasBeenClicked = false; // New variable to track if the button has been clicked
+let mainPageClicked = false; // New variable to track if the button has been clicked
+const audioElements = {};
+
 
 const sounds = {
   N: ["24-piano-keys/key13.mp3?v=1"],
@@ -29,18 +36,73 @@ const sounds = {
   '\u0060': ["24-piano-keys/key13.mp3?v=1"],
 };
 
-const audioElements = {};
+function getRandomColor() {
+  const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF"]; // Add your desired colors here
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function updateCursorColor() {
+  const randomColor1 = getRandomColor();
+  const randomColor2 = getRandomColor();
+  cursorSquare.style.setProperty("--gradient-colors", `${randomColor1}, ${randomColor2}`);
+}
+
+function updateCustomCursor(event) {
+  const { clientX, clientY } = event;
+  customCursor.style.left = `${clientX}px`;
+  customCursor.style.top = `${clientY}px`;
+
+  // Get the computed color of the button
+  const computedStyle = window.getComputedStyle(button);
+  const buttonColor = computedStyle.getPropertyValue('background-color');
+  customCursor.style.backgroundColor = buttonColor;
+}
+
+document.addEventListener("mousemove", updateCustomCursor);
+
+function showLetterButton(letter) {
+  mainPage.textContent = letter;
+  mainPage.style.visibility = "visible";
+}
+
+text.addEventListener("mouseover", (event) => {
+  const target = event.target;
+  if (target.tagName === "SPAN") {
+    showLetterButton('Enter');
+  }
+});
+
+
+function followMouse(event) {
+  // Get the mouse position
+  const mouseX = event.pageX;
+  const mouseY = event.pageY;
+
+  // Calculate the position for the cursor square
+  const cursorX = mouseX - cursorSquare.clientWidth / 2;
+  const cursorY = mouseY - cursorSquare.clientHeight / 1;
+
+  // Set the position for the cursor square
+  cursorSquare.style.left = `${cursorX}px`;
+  cursorSquare.style.top = `${cursorY}px`;
+
+  // Check if the mouse is within the button area
+  const buttonRect = button.getBoundingClientRect();
+  const isHoveringButton = mouseX >= buttonRect.left && mouseX <= buttonRect.right && mouseY >= buttonRect.top && mouseY <= buttonRect.bottom;
+
+  // Set a class to the cursor square to apply a different style when hovering over the button
+  cursorSquare.classList.toggle("hovering-button", isHoveringButton);
+}
 
 Object.entries(sounds).forEach(([letter, audioUrls]) => {
   const audios = audioUrls.map((audioUrl) => {
     const audio = new Audio(audioUrl + "?v=1");
-    audio.preload = "auto";
     return audio;
   });
   audioElements[letter] = audios;
 });
 
-// Add a user interaction event to allow audio playback
+
 function playAudio(letter) {
   if (!isPaused && audioElements[letter]) {
     const audioUrls = audioElements[letter];
@@ -54,38 +116,121 @@ function playAudio(letter) {
   }
 }
 
-function initialize() {
-  if (!isInitialized) {
-    const hoverEffect = () => {
-      const letters = text.textContent.split("");
-      text.textContent = "";
+function preloadAudio(letter) {
+  return new Promise((resolve, reject) => {
+    if (audioElements[letter]) {
+      const audioUrls = audioElements[letter];
+      let loadedCount = 0;
+      audioUrls.forEach((audioUrl) => {
+        const audio = new Audio(audioUrl + "?v=1");
+        audio.load();
+        audio.addEventListener("loadeddata", () => {
+          loadedCount++;
+          if (loadedCount === audioUrls.length) {
+            resolve();
+          }
+        });
+        audio.addEventListener("error", (error) => {
+          reject(error);
+        });
+      });
+    } else {
+      reject(new Error(`Audio files for letter ${letter} not found.`));
+    }
+  });
+}
+
+async function showLettersAndSymbols() {
+  if (!hasBeenClicked) {
+    const letters = text.textContent.split("");
+    text.textContent = "";
+    try {
+      await Promise.all(letters.map((letter) => preloadAudio(letter)));
       letters.forEach((letter) => {
         const span = document.createElement("span");
         span.textContent = letter;
         span.addEventListener("mouseover", () => {
           span.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-          playAudio(letter); // Call the playAudio function on mouseover
+          playAudio(letter);
         });
         text.appendChild(span);
       });
-    };
-    hoverEffect();
+      isInitialized = true;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+function changeLetterColorRandomly() {
+  if (!hasBeenClicked) {
+    const letters = text.querySelectorAll("span");
+    letters.forEach((letter) => {
+      letter.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    });
+    hasBeenClicked = true;
+  }
+}
+function showLettersAndSymbols() {
+  if (!isInitialized) {
+    const letters = text.textContent.split("");
+    text.textContent = "";
+    letters.forEach((letter) => {
+      const span = document.createElement("span");
+      span.textContent = letter;
+      span.addEventListener("mouseover", () => {
+        span.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        playAudio(letter);
+      });
+      text.appendChild(span);
+      preloadAudio(letter); // Preload the audio for the corresponding letter
+    });
     isInitialized = true;
   }
 }
 
-button.addEventListener("click", () => {
+function toggleButtonAnimation() {
   button.classList.toggle("spin");
   isPaused = !isPaused;
-  if (!isPaused) {
-    initialize();
-  }
-});
+}
 
-// Bounce animation on click for button 1
-button.addEventListener("click", () => {
-  button.classList.add("bounce");
-  setTimeout(() => {
-    button.classList.remove("bounce");
-  }, 1000);
-});
+function handleButtonClick() {
+  if (!hasBeenClicked) {
+    showLettersAndSymbols();
+    changeLetterColorRandomly();
+  }
+
+  toggleButtonAnimation();
+  
+}
+
+button.addEventListener("click", handleButtonClick);
+
+
+
+function handleNewPage() {
+  if (!mainPageClicked) {
+    // Start the transition animation
+    const expandedCursor = document.createElement("div");
+    expandedCursor.classList.add("expanded-cursor");
+    document.body.appendChild(expandedCursor);
+
+    const transitionBg = document.createElement("div");
+    transitionBg.classList.add("transition-bg");
+    document.body.appendChild(transitionBg);
+
+    // Use the "transitionend" event to detect when the transition is completed
+    transitionBg.addEventListener("transitionend", () => {
+      // After the transition is complete, navigate to the new page
+      window.location.href = "mainpage.html";
+    });
+
+    // Update the opacity to make the transition background appear
+    setTimeout(() => {
+      transitionBg.style.opacity = "1";
+    }, 200); // Delay the opacity change to allow the cursor animation to start
+  }
+}
+
+const letterButton = document.querySelector(".letter-button");
+letterButton.addEventListener("click", mainPageClicked);
